a289ef63f925adf8af49b37dc16b981c7b73ca05
Stella McEwen <mcewen.stella@gmail.com>
2019-04-06 18:06:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0 
     "Buzz"
-    elsif int % 5 ==0 && int % 3 == 0 
+    elsif int % 5 == 0 && int % 3 == 0 
     "FizzBuzz"
-  elsif int % 5 
+  elsif int % 5 != 0 
     "nil"
 end
 end
